61de16fd7f9084ff38c70275278e6239aea695a1
futoase <futoase@gmail.com>
2011-11-17 23:09:55 +0900

    add web directory.

---
+++ web/drossel.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return 'Fire Ball'
+
+if __name__ == '__main__':
+  app.run()
