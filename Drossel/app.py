9344052b4059f2c783c0ae59636a2ae93cee26a7
futoase <futoase@gmail.com>
2011-11-28 03:16:02 +0900

    create view mock up.

--- Drossel/app.py
+++ Drossel/app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return 'Fire Ball'
+  return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
