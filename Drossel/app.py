2ba7e070a6efaf79a823622622d702ea4e3dc6db
jkomi <jkomi@www6053ua.sakura.ne.jp>
2012-02-07 22:44:18 +0900

    [Drossel] flask->bottle

--- Drossel/app.py
+++ Drossel/app.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from flask import Flask, render_template
+from bottle import route, run
 
-app = Flask(__name__)
-
-@app.route("/")
+@route("/")
 def index():
-  return render_template('index.html')
+  return "index"
+  #return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  run()
