56e747c57fc208b31b515f5ac9b86d960bf3bfa9
jkomi <jkomi@www6053ua.sakura.ne.jp>
2012-02-07 22:33:22 +0900

    [gedechitnis] flask->bottl

--- Gedechitnis/gedechitnis.py
+++ Gedechitnis/gedechitnis.py
@@ -3,26 +3,20 @@
 
 #exec-web api
 
-from flask import Flask
-from flask import request,jsonify
+from bottle import route, run, request
 
 import exec_engine as e
 
-app = Flask(__name__)
-
-@app.route("/exec", methods=['GET','POST'])
+@route("/exec", methods=['GET','POST'])
 def index():
   if request.method in ['GET','POST']:
-  #if request.method == 'POST' or True:
-    #return str(request.form.keys())
     src = request.args.get('src')
     lang = request.args.get('lang')
     engine = e.ExecEngine()
-    #return str((src,lang))
     output = engine.execCode(src, lang)
-    return jsonify(stdout=output)
+    return output
   else:
     return 'error-request not post'
 
 if __name__ == '__main__':
-  app.run(debug=True, port=8080)
+  run(port=6677)
